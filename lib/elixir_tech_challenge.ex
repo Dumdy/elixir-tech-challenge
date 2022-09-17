0ab3a880f7c601544415d1e2b96628bfd10e08c8
dumdy <dumadibakor@gmail.com>
2022-09-17 21:54:52 +0100

    initial commite

---
+++ lib/elixir_tech_challenge.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTechChallenge do
+  @moduledoc """
+  Documentation for `ElixirTechChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTechChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
