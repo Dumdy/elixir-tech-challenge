ad365332494831bce759662a20b51893c01dcf06
dumdy <dumadibakor@gmail.com>
2022-09-19 10:11:29 +0100

    tested

--- test/elixir_tech_challenge_test.exs
+++ test/elixir_tech_challenge_test.exs
@@ -2,6 +2,7 @@ defmodule ElixirTechChallengeTest do
   use ExUnit.Case
 
   test "test reservation output" do
+    #runnning the test requires you to hit the enter button consistently on test result is displayed in the terminal
     value = ElixirTechChallenge.get_all_reservations()
 
     assert ElixirTechChallenge.get_all_reservations() == value
